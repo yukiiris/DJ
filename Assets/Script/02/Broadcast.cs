c37037858e0ecacbbd8b9e9d2591a326c570db41
moltres <25852469@qq.com>
2018-01-26 22:30:21 +0800

    小游戏

---
+++ Assets/Script/02/Broadcast.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Broadcast : MonoBehaviour {
+	public Text text;
+	public Text text2;
+	public string words;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(f());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator f()
+	{
+		string t = " ";
+		for (int i = 0; i < words.Length; i++)
+		{
+			text.text = words.Substring(0, i);
+			if (i > 3)
+			{
+				text2.text = words.Substring(0, i - 3);
+			}
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+}
