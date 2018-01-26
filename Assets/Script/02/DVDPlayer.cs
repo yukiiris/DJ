06dc623032e00f7349a3e9ea853f9cd59e18f78a
moltres <25852469@qq.com>
2018-01-27 05:06:46 +0800

    dvd

--- Assets/Script/02/DVDPlayer.cs
+++ Assets/Script/02/DVDPlayer.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DVDPlayer : MonoBehaviour {
-
+	public Text title;
 	// Use this for initialization
 	void Start () {
 		
@@ -16,9 +17,17 @@ public class DVDPlayer : MonoBehaviour {
 
 	private void OnTriggerStay(Collider other)
 	{
+		other.transform.gameObject.GetComponent<Disk>().isUsed = true;
 		if (!other.transform.gameObject.GetComponent<Disk>().isClicked)
 		{
 			other.transform.position = new Vector3(10, 10, 0);
+			title.text = other.transform.GetComponent<Disk>().title;
+
 		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		other.transform.gameObject.GetComponent<Disk>().isUsed = false;
 	}
 }
--- Assets/Script/02/Disk.cs
+++ Assets/Script/02/Disk.cs
@@ -7,9 +7,11 @@ public class Disk : MonoBehaviour {
 	public AudioSource music;
 	public string info;
 	public bool isClicked = false;
+	public bool isUsed = false;
+	public Vector3 old;
 	// Use this for initialization
 	void Start () {
-		
+		old = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -25,5 +27,9 @@ public class Disk : MonoBehaviour {
 	private void OnMouseUp()
 	{
 		isClicked = false;
+		if (!isUsed)
+		{
+			transform.position = old;
+		}
 	}
 }
