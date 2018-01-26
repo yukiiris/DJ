9589c894895642a22a35cc43d7eb3e328e7c8b14
moltres <25852469@qq.com>
2018-01-27 02:06:36 +0800

    翻页、信和日报

---
+++ Assets/Script/01/PageFilperN.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageFilperN : MonoBehaviour {
+	public Text title;
+	public Text content;
+	public string[] titles;
+	public string[] contents;
+	public int index = 1;
+
+	// Use this for initialization
+	void Start () {
+		title.text = titles[index - 1];
+		content.text = contents[index - 1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnMouseDown()
+	{
+		if (index < titles.Length)
+		{
+			index++;
+			title.text = titles[index - 1];
+			content.text = contents[index - 1];
+		}
+	}
+}
---
+++ Assets/Script/01/PageFilperP.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageFilperP : MonoBehaviour
+{
+	public Text title;
+	public Text content;
+	public string[] titles;
+	public string[] contents;
+	public PageFilperN p;
+	int index;
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		index = p.index;
+	}
+
+	private void OnMouseDown()
+	{
+		if (index > 1)
+		{
+			index--;
+			p.index--;
+			title.text = titles[index - 1];
+			content.text = contents[index - 1];
+		}
+	}
+}
