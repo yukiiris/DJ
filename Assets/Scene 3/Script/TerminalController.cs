502856a7084d346135a57d9aa5ac3856ee722b26
chiellini <18319277690@163.com>
2018-01-26 23:01:09 +0800

    scene 3 initial

---
+++ Assets/Scene 3/Script/TerminalController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[AddComponentMenu]
+public class TerminalController : MonoBehaviour {
+
+    private void OnMouseUp()
+    {
+        StaticValue.currentChoosingPage = StaticValue.ChoosingState.terminal;
+    }
+}
