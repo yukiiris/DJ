338ccb8a98c312cf827a27450238974c04c98abf
chiellini <18319277690@163.com>
2018-01-27 05:31:03 +0800

    moneysystem initial

---
+++ Assets/Script/03/ExitShowingButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitShowingButton : MonoBehaviour {
+
+    private void OnMouseUp()
+    {
+        StaticValue.currentChoosingPage = StaticValue.ChoosingState.scene_3_mainPage;
+        StaticValue.hasShowing = false;
+        Destroy(transform.parent.gameObject);    }
+}
