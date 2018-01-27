7bfb545b8927d887b9ec22591acf659ff924aab0
moltres <25852469@qq.com>
2018-01-27 17:02:41 +0800

    111

---
+++ Assets/Script/02/StopDVD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopDVD : MonoBehaviour {
+	public GameObject[] g;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnMouseDown()
+	{
+		if (DVDPlayer.c == null)
+		{
+			return;
+		}
+		DVDPlayer.c.GetComponent<Disk>().music.Stop();
+		foreach (GameObject gg in g)
+		{
+			gg.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Script/Database/Database.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Database : MonoBehaviour {
+
+	string[] terminal;
+	string[] news;
+	string[] tabliod;
+}
