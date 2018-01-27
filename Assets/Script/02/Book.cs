69900d2d5eba44eca14a9c0b6eafcc2b6d762f0d
moltres <25852469@qq.com>
2018-01-27 10:58:56 +0800

    111

--- Assets/Script/02/Book.cs
+++ Assets/Script/02/Book.cs
@@ -2,29 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Disk : MonoBehaviour {
+public class Book : MonoBehaviour {
+
 	public string title;
-	public AudioSource music;
 	public string info;
 	public bool isClicked = false;
 	public bool isUsed = false;
 	public Vector3 old;
-	int layer;
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		old = transform.position;
-		layer = GetComponent<SpriteRenderer>().sortingOrder;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
 
 	private void OnMouseDown()
 	{
 		isClicked = true;
-		gameObject.GetComponent<SpriteRenderer>().sortingOrder = 6;
 	}
 
 	private void OnMouseUp()
@@ -34,6 +33,5 @@ public class Disk : MonoBehaviour {
 		{
 			transform.position = old;
 		}
-		gameObject.GetComponent<SpriteRenderer>().sortingOrder = layer;
 	}
 }
---
+++ Assets/Script/02/BookPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookPlayer : MonoBehaviour {
+
+	public Text title;
+	public Text content;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		other.transform.gameObject.GetComponent<Book>().isUsed = true;
+		if (!other.transform.gameObject.GetComponent<Book>().isClicked)
+		{
+			other.transform.position = new Vector3(10, 10, 0);
+			title.text = other.transform.GetComponent<Book>().title;
+			content.text = other.transform.GetComponent<Book>().info;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		other.transform.gameObject.GetComponent<Book>().isUsed = false;
+	}
+}
