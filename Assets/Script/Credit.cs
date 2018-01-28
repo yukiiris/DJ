7bbc7f0bd62118e809f4bc28e6dc51fcb952ffdf
moltres <25852469@qq.com>
2018-01-28 11:03:31 +0800

    场景3

--- Assets/Script/Credit.cs
+++ Assets/Script/Credit.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Credit : MonoBehaviour {
 
-	public static int credit = 8;
+	public static int credit = 10;
+	public static int left = 0;
+	public static int mag = 10;
+	public static int meal = 10;
+	public static int warm = 10;
+	public	Text text1;
+	public Text text2;
+	public Text text3;
+	public Text text4;
+	public Text text5;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +22,10 @@ public class Credit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		text1.text = "今日收入  " + credit;
+		text2.text = "订阅杂志  " + mag;
+		text3.text = "饮食       " + meal;
+		text4.text = "暖气       " + warm;
+		text5.text = "共计     " + (left + credit - mag - meal - warm);
 	}
 }
